5f0af0196a2067db24d86d740f216d9986db725f
Ruben7886 <cofr050209hmcrrba4@bggem.mx78m>
2025-10-16 09:41:50 -0600

    codigo cambios en el login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
   },
+  
   {
     path: '',
     redirectTo: 'auth/login',
